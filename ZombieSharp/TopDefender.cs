15f308795ba285126b8333c98075e1f5c6e992b4
Oylsister <57207701+oylsister@users.noreply.github.com>
2024-02-02 17:07:25 +0700

    Initial TopDefender

---
+++ ZombieSharp/TopDefender.cs
@@ -0,0 +1,25 @@
+﻿namespace ZombieSharp
+{
+    public partial class ZombieSharp
+    {
+        public Dictionary<int, ClientDamage> ClientsDamage = new();
+
+        public void TopDenfederOnRoundEnd()
+        {
+            foreach (var client in Utilities.GetPlayers())
+            {
+                if (!client.IsValid)
+                    continue;
+
+                if (!ClientsDamage.ContainsKey(client.Slot))
+                    continue;
+            }
+        }
+    }
+}
+
+public class ClientDamage
+{
+    public int Damage { get; set; }
+    public int Infected { get; set; }
+}
