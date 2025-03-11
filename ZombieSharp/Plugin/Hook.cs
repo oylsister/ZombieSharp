28434aa253a1f2544acfab6ee3ce9e6c94f60a1f
Oylsister <57207701+oylsister@users.noreply.github.com>
2025-03-11 10:25:17 +0700

    Attempt fix player spawn in void.

--- ZombieSharp/Plugin/Hook.cs
+++ ZombieSharp/Plugin/Hook.cs
@@ -207,34 +207,22 @@ public class Hook(ZombieSharp core, Weapons weapons, Respawn respawn, ILogger<Zo
         if(client == null)
             return HookResult.Continue;
 
+        if(Utils.IsPlayerAlive(client))
+            client.CommitSuicide(false, true);
+
         //Server.PrintToChatAll($"{client.PlayerName} is doing {info.GetArg(0)} {info.GetArg(1)}");
 
         var team = (CsTeam)int.Parse(info.GetArg(1));
 
         // for spectator case we allow this 
         if(team == CsTeam.Spectator || team == CsTeam.None)
-        {
-            if(Utils.IsPlayerAlive(client))
-                client.CommitSuicide(false, true);
-
             client.SwitchTeam(CsTeam.Spectator);
-        }
 
-        else
+        else if(client.Team == CsTeam.Spectator || client.Team == CsTeam.None)
         {
-            if((GameSettings.Settings?.RespawnEnable ?? true) && (GameSettings.Settings?.AllowRespawnJoinLate ?? true))
-            {
-                if(team == client.Team)
-                {
-                    client.PrintToChat("You're choosing the same team!");
-                    return HookResult.Continue;
-                }
-
-                if(Utils.IsPlayerAlive(client))
-                    client.CommitSuicide(false, true);
-
-                client.SwitchTeam(team);
-            }
+            // we spawn a player here.
+            if(GameSettings.Settings?.AllowRespawnJoinLate ?? false)
+                Infect.SpawnPlayer(client);
         }
 
         return HookResult.Continue;
