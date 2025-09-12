15e1982d32c49ccd46c0ddb442e3599766a371ef
Samir Neupane <97087641+SyntX34@users.noreply.github.com>
2025-09-12 07:32:19 +0545

    Updated API

--- ZombieSharpAPI/ZombieSharpAPI.cs
+++ ZombieSharpAPI/ZombieSharpAPI.cs
@@ -7,7 +7,9 @@ public interface IZombieSharpAPI
     public event Func<CCSPlayerController, CCSPlayerController?, bool, bool, HookResult?>? OnClientInfect;
     public event Func<CCSPlayerController, bool, HookResult?>? OnClientHumanize;
 
+    public void Hook_OnInfectClient(Func<CCSPlayerController, CCSPlayerController?, bool, bool, bool, HookResult> handler);
+
     public bool ZS_IsClientHuman(CCSPlayerController client);
-    public bool ZS_IsClientInfect(CCSPlayerController client);
+    public bool ZS_IsClientZombie(CCSPlayerController controller);
     public void ZS_RespawnClient(CCSPlayerController client);
 }
